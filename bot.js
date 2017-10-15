a72cc640115bcfa8b9313b98afa91db5bbb0a55a
carljohnson1992 <32619431+carljohnson1992@users.noreply.github.com>
2017-10-15 12:07:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,6 +87,31 @@ if (message.content === 'wednesday') {
 
         message.channel.send('<@238758020182441984>day');
               };
+    if (message.content === "timball") {
+    	var sayings = ["It is certain",
+										"It is decidedly so",
+										"Without a doubt",
+										"Yes, definitely",
+										"You may rely on it",
+										"As I see it, yes",
+										"Most likely",
+										"Outlook good",
+										"Yes",
+										"Signs point to yes",
+										"Reply hazy try again",
+										"Ask again later",
+										"Better not tell you now",
+										"Cannot predict now",
+										"Concentrate and ask again",
+										"Don't count on it",
+										"My reply is no",
+										"My sources say no",
+										"Outlook not so good",
+										"Very doubtful"];
+
+			var result = Math.floor((Math.random() * sayings.length) + 0);
+			bot.reply(message, sayings[result]);
+    };
 });
 
 // Log our bot in
